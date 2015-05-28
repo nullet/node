9761c1125604a3cbd1f0b8f1cc59659b1cef0a52
nullet <tomnullet@gmail.com>
2015-05-28 07:50:03 -0500

    kohactive route works

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var kohactive = require('./routes/kohactive');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/kohactive', kohactive);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
